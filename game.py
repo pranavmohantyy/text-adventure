944aa13408102a3339824304253aa0118acf787a
pranavmohantyy <mohantypranav147@gmail.com>
2026-06-28 08:30:10 -0700

    add basic combat system

--- game.py
+++ game.py
@@ -4,6 +4,7 @@ class Room:
         self.description = description
         self.connections = {}
         self.items = []
+        self.npcs = []
 
     def connect(self, direction, room):
         self.connections[direction] = room
@@ -17,23 +18,30 @@ class Room:
     def remove_item(self, item):
         self.items.remove(item)
 
+    def add_npc(self, npc):
+        self.npcs.append(npc)
+
 class Player:
     def __init__(self, start_room):
         self.current_room = start_room
         self.inventory = []
+        self.health = 100
 
     def move(self, direction):
-        next_room = self.current_room.get_room(direction)
-        if next_room:
-            self.current_room = next_room
+        new_room = self.current_room.get_room(direction)
+        if new_room:
+            self.current_room = new_room
+
+    def attack(self, npc):
+        if npc in self.current_room.npcs:
+            npc.health -= 10
+            if npc.health <= 0:
+                self.current_room.npcs.remove(npc)
 
 class NPC:
-    def __init__(self, name, dialogues):
+    def __init__(self, name, health):
         self.name = name
-        self.dialogues = dialogues
-        self.dialogue_index = 0
+        self.health = health
 
-    def talk(self):
-        line = self.dialogues[self.dialogue_index]
-        self.dialogue_index = (self.dialogue_index + 1) % len(self.dialogues)
-        return line
+    def attack(self, player):
+        player.health -= 10
